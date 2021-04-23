f4fd0cab5526c9b505343015154138a17714f1a7
Rok <rok.bacho@gmail.com>
2021-04-23 12:07:31 +0200

    Dodaj filme

--- actor_image_upload.php
+++ actor_image_upload.php
@@ -5,10 +5,10 @@ include_once "db.php";
 $id = (int) $_POST['id'];
 $title = $_POST['title'];
 
-/*v katero mapo, bo naložil sliko*/
+//v katero mapo bo naložil sliko
 $target_dir = "./images/";
 //random name generator
-$random = $id.'_'.date("YmdHis")."_".rand(10,10000).'_';
+$random = $id.'-'.date("YmdHis")."-".rand(10,10000).'-';
 $target_file = $target_dir . $random . basename($_FILES["file"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
@@ -34,20 +34,17 @@ if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg
   $uploadOk = 0;
 }
 
-// Check if $uploadOk is set to 1 - vse ok
+// Check if $uploadOk is set to 1 - vse je ok
 if ($uploadOk == 1) {
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+        //datoteko je prestavil, zapišemo v bazo
+        $query = "INSERT INTO actors_images(title,url,actor_id) VALUES(?,?,?)";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([$title,$target_file,$id]);
 
-  if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
-      //datoteko je prestavil, zapišemo v bazo
-      $query = "INSERT INTO actors_images(title,url,actor_id) VALUES(?,?,?)"
-      $stmt = $pdo->prepare($query);
-      $stmt->execute([$title,$target_file,$id]);
-
-  } else {
-        echo "Sorry, there was an error uploading your file.";
-  }
+    } 
 }
-
+//preusmerim nazaj na igralca
 header("Location: actor.php?id=".$id); die();
 
 ?>
